def8db6d9db70feaff6a0dcccbeaf9ae483d5bbe
João Gabriel Araujo Jorge <joaog.caj@gmail.com>
2020-08-05 17:32:57 -0300

    class Pin

---
+++ InvasaoAlienigena/Model/Pin.swift
@@ -0,0 +1,19 @@
+//
+//  Pin.swift
+//  InvasaoAlienigena
+//
+//  Created by João Gabriel Araujo Jorge on 05/08/20.
+//  Copyright © 2020 Juliana Prado. All rights reserved.
+//
+
+import Foundation
+
+class Pin {
+    var nomeCidade: String
+    let nomeImagem: String
+    
+    init(nomeCidade: String) {
+        self.nomeCidade = nomeCidade
+        self.nomeImagem = "mappin.png"
+    }
+}
