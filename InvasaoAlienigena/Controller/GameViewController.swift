63b389337247d0e76ff3a121f0c4b1cd3fe24b5c
Juliana Prado <julianaprado@me.com>
2020-08-06 19:11:27 -0300

    tentando fazer multipeer pt 1

--- InvasaoAlienigena/Controller/GameViewController.swift
+++ InvasaoAlienigena/Controller/GameViewController.swift
@@ -11,22 +11,30 @@ import SpriteKit
 import GameplayKit
 import MultipeerConnectivity
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController,MCSessionDelegate, MCBrowserViewControllerDelegate {
 
+    var peerID: MCPeerID!
+    var session: MCSession!
+    var mcAdvertiserAssistant: MCAdvertiserAssistant!
+    var messageToSend: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let session = MCSession(peer: MCPeerID(displayName: "Mary"))
+        session = MCSession(peer: MCPeerID(displayName: "Mary"))
         let serviceType = "io-objc-mpc" // Limited to 15 ASCII characters
         let browser = MCBrowserViewController(serviceType: serviceType, session: session)
         self.present(browser, animated: true, completion: nil)
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: serviceType, discoveryInfo: nil, session: session)
+        mcAdvertiserAssistant.start()
+        present(browser, animated: true)
+ 
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
@@ -42,4 +50,46 @@ class GameViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .connected:
+            print("Connected: \(peerID.displayName)")
+
+        case .connecting:
+            print("Connecting: \(peerID.displayName)")
+
+        case .notConnected:
+            print("Not Connected: \(peerID.displayName)")
+
+        @unknown default:
+            print("Unknown state received: \(peerID.displayName)")
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+
 }
