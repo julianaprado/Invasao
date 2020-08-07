3fd73f628307e171d285f15b0d6cc35213e5eb71
Juliana Prado <julianaprado@me.com>
2020-08-07 14:44:27 -0300

    advertiser e browser com navigationItem(não aparece)

--- InvasaoAlienigena/Controller/GameViewController.swift
+++ InvasaoAlienigena/Controller/GameViewController.swift
@@ -13,32 +13,49 @@ import MultipeerConnectivity
 
 class GameViewController: UIViewController,MCSessionDelegate, MCBrowserViewControllerDelegate {
 
-    var peerID: MCPeerID!
+    var peerID = MCPeerID(displayName: UIDevice.current.name)
     var session: MCSession!
     var mcAdvertiserAssistant: MCAdvertiserAssistant!
     var messageToSend: String!
+    let serviceType = "aliens-iegame"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        session = MCSession(peer: MCPeerID(displayName: "Mary"))
-        let serviceType = "io-objc-mpc" // Limited to 15 ASCII characters
-        let browser = MCBrowserViewController(serviceType: serviceType, session: session)
-        self.present(browser, animated: true, completion: nil)
-        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: serviceType, discoveryInfo: nil, session: session)
-        mcAdvertiserAssistant.start()
-        present(browser, animated: true)
- 
+        session = MCSession(peer: peerID)
+        session?.delegate = self
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
+
     }
     
+    func startHosting(action: UIAlertAction) {
+        guard let session = session else { return }
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: serviceType, discoveryInfo: nil, session: session)
+        mcAdvertiserAssistant?.start()
+    }
+
+    func joinSession(action: UIAlertAction) {
+        guard let session = session else { return }
+        let browser = MCBrowserViewController(serviceType: serviceType, session: session)
+        browser.delegate = self
+        present(browser, animated: true, completion: nil)
+    }
+    
+    @objc func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -52,23 +69,23 @@ class GameViewController: UIViewController,MCSessionDelegate, MCBrowserViewContr
     }
     
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-        switch state {
-        case .connected:
-            print("Connected: \(peerID.displayName)")
+         switch state {
+           case .connected:
+               print("Connected: \(peerID.displayName)")
 
-        case .connecting:
-            print("Connecting: \(peerID.displayName)")
+           case .connecting:
+               print("Connecting: \(peerID.displayName)")
 
-        case .notConnected:
-            print("Not Connected: \(peerID.displayName)")
+           case .notConnected:
+               print("Not Connected: \(peerID.displayName)")
 
-        @unknown default:
-            print("Unknown state received: \(peerID.displayName)")
-        }
+           @unknown default:
+               print("Unknown state received: \(peerID.displayName)")
+           }
     }
     
     func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
-        
+      
     }
     
     func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
@@ -84,11 +101,11 @@ class GameViewController: UIViewController,MCSessionDelegate, MCBrowserViewContr
     }
     
     func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-        dismiss(animated: true)
+         dismiss(animated: true)
     }
     
     func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-        dismiss(animated: true)
+         dismiss(animated: true)
     }
     
 
